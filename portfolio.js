39d72407d951ef543cf6b67cbadd0218a4ac0e8e
sabernstein <65577560+sabernstein@users.noreply.github.com>
2020-11-30 14:42:08 -0800

    firstcommit

---
+++ portfolio.js
@@ -0,0 +1,59 @@
+//const text = document.querySelector(".SADIE");
+//const string = text.textContent;
+//const splitText = string.split("");
+//text.textContent = "";
+var current0 = "right0";
+var current1 = "right1";
+var current2 = "right2";
+
+const logo = document.querySelectorAll('#logo path');
+console.log(logo);
+
+for(let i=0; i<15; i++){
+  console.log('Letter ' + i + 'is ' + logo[i].getTotalLength());
+}
+
+function switchDown(i) {
+  if (i == 0) {
+    if(current0 == "right0"){
+      current0 = "down0";
+      document.getElementById('right0').innerHTML = '<i class="material-icons" id="down0">keyboard_arrow_down</i>';
+      document.getElementById('grid-container').innerHTML = "";
+    }
+    else {
+      current0 = "right0";
+      document.getElementById('right0').innerHTML = '<i class="material-icons" id="right0">keyboard_arrow_right</i>';
+      document.getElementById('grid-container').innerHTML = "";
+    }
+  }
+  else if (i == 1) {
+    if(current1 == "right1"){
+      current1 = "down1";
+      document.getElementById('right1').innerHTML = '<i class="material-icons" id="down1">keyboard_arrow_down</i>';
+      document.getElementById('pulldown-menu-fresh').innerHTML = '<li>Multivariable Differential Calculus (MATH 230-1)</li><li>Fundamentals of Computer Programming I (COMP_SCI 111)</li><li>Environment and Society (ENVR_POL 212)</li><li>Principles of Entrepreneurship (ENTREP 225/IEMS 225)</li><li>The Nature of Plants (BIOL_SCI 109-0)</li><li>Linear Algebra (MATH 240-0)</li><li>Fundamentals of Computer Programming II (COMP_SCI 211)</li><li>Ocean and Coastal Law and Policy (ENVR_POL 390-0-24)</li>';
+    }
+    else {
+      current1 = "right1";
+      document.getElementById('right1').innerHTML = '<i class="material-icons" id="right1">keyboard_arrow_right</i>';
+      document.getElementById("pulldown-menu-fresh").innerHTML="";
+    }
+  }
+  else {
+    if(current2 == "right2"){
+      current2 = "down2";
+      document.getElementById('right2').innerHTML = '<i class="material-icons" id="down2">keyboard_arrow_down</i>';
+      document.getElementById("pulldown-menu-soph").innerHTML =
+      "<li>Data Structures and Data Management (COMP_SCI 214)</li><li>Human Computer Interaction (COMP_SCI 330)</li>";
+    }
+    else {
+      current2 = "right2";
+      document.getElementById('right2').innerHTML = '<i class="material-icons" id="right2">keyboard_arrow_right</i>';
+      document.getElementById("pulldown-menu-soph").innerHTML="";
+    }
+  }
+}
+
+function done(){
+  clearInterval(timer);
+  time = Null;
+}
